0d0174ed736ee30fa048aa5821df40c6b2a467b2
richardhill25 <richard_hill25@hotmail.com>
2021-03-20 16:37:18 +1100

    Update TradingEngineMain.java
    Add some comments

--- TradingEngine/src/main/java/com/ricktech/application/TradingEngineMain.java
+++ TradingEngine/src/main/java/com/ricktech/application/TradingEngineMain.java
@@ -3,6 +3,7 @@ package com.ricktech.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Entry point for the Trading Engine application. Run the main to launch the application.
 @SpringBootApplication
 public class TradingEngineMain {
     public static void main(String[] args) {
